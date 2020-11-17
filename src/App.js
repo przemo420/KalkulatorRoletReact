36121fd6482e0e8eb6c1b8964722e9a399846400
JohnyRLK <jan.rycielski@gmail.com>
2020-11-17 10:39:19 +0100

    commit first react kalkulator

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <div classNameName = "App">
+    <header>
+    </header> 
+    <body>
+   <div className="all_holder">
+     <div className="column_left">
+       <div className="first_window">
+         <div className="window_photo"></div>
+       </div>
+       <div className="secend_window">
+         <div className="blind_section">
+           <div className="blind">
+             <h2>Roleta 1</h2>
+           </div>
+           <div className="blind">
+             <h2>Roleta 1</h2>
+           </div>
+         </div>
+         <div className="price_section">
+           <div className="price">
+             <p>Cena:</p>
+             <p>123zł</p>
+           </div>
+           <div className="price">
+             <p>Cena:</p>
+             <p>123zł</p>
+           </div>
+         </div>
+       </div>
+     </div>
+     <p>Hello, world!</p>
+   </div>
+ </body> 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+    );
 }
 
 export default App;
