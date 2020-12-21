90492ccbee89c2a4fe933789d1a4395270e19142
Przemysław Kozłowski <kozioll.mozoll@gmail.com>
2020-12-21 19:28:29 +0100

    update

--- src/src/components/Blind/Installation.js
+++ src/src/components/Blind/Installation.js
@@ -1,31 +1,32 @@
 import React from 'react'
+
 export class Installation extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.handleType = [
-            {'name': 'Montaż bezinwazyjny standard', 'img': require( '../../images/handle/first.jpg' ), 'price': 0},
-            {'name': 'Montaż bezinwazyjny do okien z nawiewnikiem', 'img': require( '../../images/handle/secend.jpg' ), 'price': 0},
-            {'name': 'Montaż inwazyjny', 'img': require( '../../images/handle/third.jpg' ), 'price': 0}
+            {'id': 'first', 'name': 'Montaż bezinwazyjny standard', 'img': 'first.jpg', 'price': 0},
+            {'id': 'second', 'name': 'Montaż bezinwazyjny do okien z nawiewnikiem', 'img': 'secend.jpg', 'price': 0},
+            {'id': 'third', 'name': 'Montaż inwazyjny', 'img': 'third.jpg', 'price': 0}
         ];
 
-        this.handleChange = this.handleChange.bind(this)
+        this.addActiveClass = this.addActiveClass.bind(this);
+        this.state = { active: null };
     }
 
-    handleChange(event) {
-
-    }
-
+    addActiveClass( i ) {
+        this.setState({ active: i });
+    };
     render() {
         return(
             <div>
                 <h4 className="mt-5">Typ montażu</h4>
                 <hr></hr>
-                <div class="row justify-content-center">
+                <div className="row justify-content-center">
                     {this.handleType.map((item, i) => (
-                       <div class="col-md-4 col-12 text-center alert alert-primary" onClick={this.handleChange}>
-                           <img src={item.img} alt={item.name} width="80%"/>
-                           <small>{item.name}</small>
-                        </div>
+                    <div className={`col-md-4 col-12 text-center ` + (this.state.active === i ? 'alert alert-primary' : null)} onClick={() => this.addActiveClass(i)} key={i}>
+                         <img src={`images/handle/`+item.img} alt={item.name} width="80%"/>
+                         <small>{item.name}</small>
+                     </div>
                     ))}
                 </div>
             </div>
